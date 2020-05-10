5ceccf0826fbb3d1d2e41784ec945d53ffc44c54
freddysilber <freddy.silber@cloudcoach.com>
2020-05-09 23:45:26 -0600

    adds axios

--- src/components/SkatersList.js
+++ src/components/SkatersList.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class SkatesList extends React.Component {
+
+	componentDidMount() {
+		axios.get('https://sk8-api.herokuapp.com/api/v1/skaters')
+			.then(data => console.log(data))
+	}
+
 	render() {
 		return (
 			<>
